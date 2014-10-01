fecc19bdbadd0cd9659926225ccbf89048f19289
Marek Suscak <m.suscak@youcon.com>
2014-10-01 14:22:48 +0200

    replace OAuth with Basic authentication flow for OnDemand instances which do not support 2 legged OAuth (2LO)

--- gitblit-jira-hook.groovy
+++ gitblit-jira-hook.groovy
@@ -8,12 +8,6 @@ import groovyx.net.http.RESTClient
 import groovyx.net.http.HttpResponseException
 import groovyx.net.http.ContentType
 
-@Grab(group='com.google.gdata', module='core', version='1.47.1')
-import com.google.gdata.client.authn.oauth.RsaSha1PrivateKeyHelper
-import com.google.gdata.client.authn.oauth.OAuthRsaSha1Signer
-import com.google.gdata.client.authn.oauth.OAuthParameters
-import com.google.gdata.client.authn.oauth.OAuthUtil
-
 /*
 
 Gitblit Post-Receive Hook: jira-comment
@@ -23,21 +17,9 @@ The purpose of this script is to invoke the jira API and add a comment to an iss
 This script is only executed when pushing to *Gitblit*, not to other Git tooling you may be using.
 
 Setup:
-1. Generate an RSA pub/priv key pair (Atlassian's OAuth provider uses RSA-SHA1 to sign the request)
- openssl genrsa 1024 | openssl pkcs8 -topk8 -nocrypt -out rsa.pem
- openssl rsa -in rsa.pem -pubout > rsa.pub
+1. Create a new Jira service user 
 
-2. Configure an Application Link.
- To register an OAuth consumer, you'll need to register an Application Link inside your Atlassian product.
-
-3. After you've created an Application Link, configure an "Incoming Authentication" with the following details:
- Consumer key:             gitblit
- Consumer name:            gitblit
- Public key:               <paste the contents of rsa.pub>
- Alllow 2-Legged OAuth     true
- Allow user impersonation  true
-
-4. Set parameters in hook according to your environment
+2. Set parameters in hook according to your environment
 
 */
 
@@ -46,11 +28,10 @@ logger.info("jira comment hook triggered by ${user.username} for ${repository.na
 Repository repo = gitblit.getRepository(repository.name)
 
 def jiraUrl = "http://localhost:5000"
-def consumerKey = "gitblit"
 def jiraRepoRegex = "\\b([A-Z]+)-(\\d+)\\b"
-def jiraCloseIssueRegex = "(resolve[sd]?|fix(es|ed)?|close[sd]?)\\s+([A-Z]+)-([0-9]+)"
+def jiraServiceUserName = "gitblit"
+def jiraServiceUserPassword = "gitblit"
 def gitwebUrl = "http://localhost:8080/gitblit"
-def privKeyFile = "/srv/gitblit/data/rsa.pem"
 
 for (command in commands) {
   def commits = JGitUtils.getRevLog(repo, command.oldId.name, command.newId.name).reverse()
@@ -70,33 +51,14 @@ for (command in commands) {
         "[$repoName $commitRev|$revUrl]\n"+
         "*message*: " + commitMessage
 
-      OAuthRsaSha1Signer rsaSigner = new OAuthRsaSha1Signer();
-      rsaSigner.setPrivateKey(RsaSha1PrivateKeyHelper.getPrivateKeyFromFilename(privKeyFile));
-
-      OAuthParameters params = new OAuthParameters();
-      params.setOAuthConsumerKey(consumerKey);
-      params.setOAuthNonce(OAuthUtil.getNonce());
-      params.setOAuthTimestamp(OAuthUtil.getTimestamp());
-      params.setOAuthSignatureMethod("RSA-SHA1");
-      params.setOAuthType(OAuthParameters.OAuthType.TWO_LEGGED_OAUTH);
-      params.setOAuthToken("");
-      params.addCustomBaseParameter("user_id", "${user.username}");
-
-      String paramString = params.getBaseParameters().sort().collect{it}.join('&')
-      String baseString = [OAuthUtil.encode("POST")
-        ,OAuthUtil.encode(jiraUrl+issueUrl)
-        ,OAuthUtil.encode(paramString)].join('&')
-
-      String signature = rsaSigner.getSignature(baseString, params);
-      params.addCustomBaseParameter("oauth_signature", signature);
-
-      logger.info("restClient.post to ${issueUrl}")
+     
+      logger.info("restClient.post to ${issueUrl} with service user ${jiraServiceUserName}")
       def restClient = new RESTClient(jiraUrl)
+      restClient.headers['Authorization'] = "Basic "+"${jiraServiceUserName}:${jiraServiceUserPassword}".getBytes('utf-8').encodeBase64()
       try {
         def resp = restClient.post(
         path : issueUrl,
         body : [body:commitMessageWithLink],
-        query : params.getBaseParameters(),
         requestContentType : ContentType.JSON )
 
         assert resp.status == 201
